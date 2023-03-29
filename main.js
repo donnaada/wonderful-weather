8ca87e00b5a61febf6059fd137ec0eef5acb432c
Donna Ada <125130284+donnaada@users.noreply.github.com>
2023-03-29 11:24:01 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let bodyEl= document.querySelector('body');
+let bodyEl = document.querySelector('body');
 let btnEl = document.getElementById('btn');
 let inputEl = document.getElementById('cityInput').value;
 let h1El = document.getElementById('h1');
@@ -9,7 +9,7 @@ let h5El = document.getElementById('h5');
 let h6El = document.getElementById('h6');
 
 
-document.addEventListener('click', function(e){
+document.addEventListener('click', function(e) {
   e.preventDefault();
   console.log(e.currentTarget);
 });
@@ -19,7 +19,7 @@ let apiUrl = 'https://api.api-ninjas.com/v1/weather?city='
 
 const options = {
   method: 'GET',
-  headers:{
+  headers: {
     'X-API-KEY': apiKey
   },
 };
@@ -27,25 +27,25 @@ const options = {
 
 let farenheit = 0;
 
-function cToF(celsius){
-  return farenheit = (celsius * 9/5) + 32;
+function cToF(celsius) {
+  return farenheit = (celsius * 9 / 5) + 32;
 }
 
-async function getWeather(){
+async function getWeather() {
   inputEl = document.getElementById('cityInput').value;
-  let city = inputEl.value;
+  let city = inputEl;
   console.log(city);
   console.log('getWeather');
 
   h1El.innerText = 'loading weather...';
   let response = await fetch(apiUrl + city, options);
   let data = await response.json();
-  h1El.innerText =city;
+  h1El.innerText = city;
   console.log(`${data.weather}, ${data.temp}`)
 
   console.log(data);
   console.log(data.temp);
-  console.log(cToF(data.temp)); d;
+  console.log(cToF(data.temp));
 }
 
 
